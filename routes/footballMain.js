4f8ee662085339b5181d3e88573261dc87b47107
Danny Fung <dannyfung97@gmail.com>
2019-02-24 12:23:50 -0800

    asdf

--- routes/footballMain.js
+++ routes/footballMain.js
@@ -4,3 +4,30 @@ exports.view = function(req, res) {
 	console.log(user);
 	res.render('footballMain', user);
 };
+
+function fetch() {
+    console.log("WORKING");
+    console.log(data.users[0]);
+    console.log("spawn: " + data.users[0].logged);
+    $.each(data.users, function (i, user) {
+        if (user.logged == "true" && user.preferences[0].check == "checked") {
+            user_index = i;
+            $.each(user.playbooks, function (j, playbook) {
+                if (playbook.selected == "true") {
+                    playbook_index = j;
+                    $.each(playbook.plays, function (k, play) {
+                        if (play.selected == "true") {
+                            play_index = k;
+                            $.each(play.positions, function (l, position) {
+                                $("#field").append('<div class="player" id="player' + l + '" onclick="dragElement(this)" ontouchstart="dragElementTouch(this)" style="position: absolute;z-index: 9; top: ' + position.top + '; left: ' + position.left + '"><img src="img/circle.png" style="width:25px; height:25px; background: ' + position.color + '; border-radius:12.5px"></div>');
+                                dragElement(document.getElementById("player" + l));
+                                dragElementTouch(document.getElementById("player" + l));
+                            });
+                        }
+                    });
+                }
+            });
+        }
+    });
+}
+exports = fetch;
