525184ca8f102719032b3211ed19ccbe1f0b3fb4
Justin Cruz <jtc027@ucsd.edu>
2019-02-25 16:36:56 -0800

    Link login to JSON

--- routes/login.js
+++ routes/login.js
@@ -14,6 +14,163 @@ exports.update = function (request, response) {
       }
       break;
   }
+
+  console.log("IM IN LOGIN UPDATE");
+      var uname = request.query.username;
+      var pword = request.query.password;
+      var newUser = {};
+
+          newUser = {
+            username: uname,
+            password: pword,
+            logged: "true",
+            preferences: [
+              {
+                sport: "football",
+                check: "checked",
+                display: "on"
+              },
+              {
+                sport: "basketball",
+                check: "checked",
+                display: "on"
+              },
+              {
+                sport: "baseball",
+                check: "checked",
+                display: "on"
+              }
+            ],
+            playbooks: [
+              {
+                name: "default",
+                selected: "true",
+                plays: [
+                  {
+                    name: "default",
+                    selected: "true",
+                    imgsrc: "img/footballField.jpg",
+                    positions: [
+                      {
+                        top: "50%",
+                        left: "28%",
+                        color: "red"
+                      },
+                      {
+                        top: "50%",
+                        left: "40%",
+                        color: "red"
+                      },
+                      {
+                        top: "50%",
+                        left: "45%",
+                        color: "red"
+                      },
+                      {
+                        top: "30%",
+                        left: "40%",
+                        color: "red"
+                      },
+                      {
+                        top: "50%",
+                        left: "55%",
+                        color: "red"
+                      },
+                      {
+                        top: "50%",
+                        left: "60%",
+                        color: "red"
+                      },
+                      {
+                        top: "50%",
+                        left: "67%",
+                        color: "red"
+                      },
+                      {
+                        top: "45%",
+                        left: "65%",
+                        color: "red"
+                      },
+                      {
+                        top: "40%",
+                        left: "55%",
+                        color: "red"
+                      },
+                      {
+                        top: "40%",
+                        left: "45%",
+                        color: "red"
+                      },
+                      {
+                        top: "30%",
+                        left: "60%",
+                        color: "red"
+                      },
+                      {
+                        top: "65%",
+                        left: "50%",
+                        color: "blue"
+                      },
+                      {
+                        top: "60%",
+                        left: "55%",
+                        color: "blue"
+                      },
+                      {
+                        top: "60%",
+                        left: "45%",
+                        color: "blue"
+                      },
+                      {
+                        top: "60%",
+                        left: "60%",
+                        color: "blue"
+                      },
+                      {
+                        top: "60%",
+                        left: "40%",
+                        color: "blue"
+                      },
+                      {
+                        top: "60%",
+                        left: "28%",
+                        color: "blue"
+                      },
+                      {
+                        top: "60%",
+                        left: "67%",
+                        color: "blue"
+                      },
+                      {
+                        top: "65%",
+                        left: "65%",
+                        color: "blue"
+                      },
+                      {
+                        top: "65%",
+                        left: "62%",
+                        color: "blue"
+                      },
+                      {
+                        top: "70%",
+                        left: "50%",
+                        color: "blue"
+                      },
+                      {
+                        top: "60%",
+                        left: "50%",
+                        color: "blue"
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          }
+          data.users.pop();
+          data.users.push(newUser);
+          console.log(data);
+
   console.log("NEW");
   console.log(data);
   response.render('index', data);
