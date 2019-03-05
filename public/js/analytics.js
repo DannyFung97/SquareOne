1337d5ff30df445e696123b8298ca1bceae1bccd
Justin Cruz <jtc027@ucsd.edu>
2019-03-04 16:40:02 -0800

    ANAL ANAL ANAL

--- public/js/analytics.js
+++ public/js/analytics.js
@@ -6,20 +6,20 @@ $(document).ready(function () {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$("#redoButton").click(redoClick);
-	$("#undoButton").click(undoClick);
+	//$("#redoButton").click(redoClick);
+	//$("#undoButton").click(undoClick);
 	$("#playButton").click(playClick);
 }
 
-function redoClick(e) {
-	e.preventDefault();
-	ga('send', 'event', 'redo', 'click');
-} 
+//function redoClick(e) {
+//	e.preventDefault();
+//	ga('send', 'event', 'redo', 'click');
+//} 
 
-function undoClick(e) {
-	e.preventDefault();
-	ga('send', 'event', 'undo', 'click');
-}
+//function undoClick(e) {
+//	e.preventDefault();
+//	ga('send', 'event', 'undo', 'click');
+//}
 
 function playClick(e) {
 	e.preventDefault();
